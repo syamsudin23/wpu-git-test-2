0af4aa1e4883252a28c1376fe98cdf6b1b501743
syam23 <symsantos7@gmail.com>
2025-02-16 18:01:43 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, illo corporis adipisci modi necessitatibus iste nisi voluptates, dolore consequuntur quaerat itaque tempore sit eligendi! Autem expedita corporis numquam nobis nihil.</p>
 </body>
 </html>
